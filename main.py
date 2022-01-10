cf9e5dfc2f7f59240939b5c7cf773477b174f1bc
w1png <wonepng@gmail.com>
2022-01-10 16:21:00 +0300

    Moved database creation to main.py

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ import bill as b
 import text_templates as tt
 import state_handler as sh
 
+conn = sqlite3.connect("data.db")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER, family_id INTEGEr)")
+c.execute("CREATE TABLE IF NOT EXISTS families (family_id INTEGER, creator_id INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS invites (user_id INTEGER, family_id INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS bills (bill_id INTEGER, family_id INTEGER, user_id INGEGER, price REAL, message TEXT, date TEXT)")
+conn.commit()
+
+
 with open("token.txt", "r") as token_file:
     token = token_file.readlines()[0]
 
